89d58ddfc219b25e39319181251e3db792120d8b
Arif Md. Shahriar Onoy <arif.shahriar@bracits.com>
2023-12-05 19:07:18 +0600

    base from jhipster

---
+++ src/test/java/io/project/jhipster/IntegrationTest.java
@@ -0,0 +1,22 @@
+package io.project.jhipster;
+
+import io.project.jhipster.JhipsterApp;
+import io.project.jhipster.config.AsyncSyncConfiguration;
+import io.project.jhipster.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { JhipsterApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
